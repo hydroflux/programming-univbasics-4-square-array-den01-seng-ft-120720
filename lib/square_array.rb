a567885c717fb59500c056799ff45cab902fc61a
Jack Hubert <jphubert93@gmail.com>
2020-11-23 21:11:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  
+  array.length.times do |index|
+    square_array.push(array[index] + 1)
 end
+ 
+# array.length.times do |index|
+#   new_array.push(array[index] + 1)
+# end
+# new_array
