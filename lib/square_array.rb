1a1bb2c9d116e3133f8c3db66d5f451fd968f8d5
Jack Hubert <jphubert93@gmail.com>
2020-11-23 21:13:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -14,10 +14,10 @@ end
 
 
 
-counter = 0
-array = [1,2,3,4,5]
+# counter = 0
+# array = [1,2,3,4,5]
  
-while array[counter] do
-  puts array[counter]
-  counter += 1
-end
+# while array[counter] do
+#   puts array[counter]
+#   counter += 1
+# end
